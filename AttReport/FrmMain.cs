f67ddb98d7474d82e7c9b00e6cf113e543e13ea8
XiaoWei <40625200+luochen210@users.noreply.github.com>
2018-12-12 17:38:18 +0800

    初步实现记录分类

--- AttReport/FrmMain.cs
+++ AttReport/FrmMain.cs
@@ -251,14 +251,36 @@ namespace AttReport
             DateTime iToday = DateTime.Parse("2018/12/10");//测试用
             //MessageBox.Show(iToday.ToString());
 
+            decimal AtDay = 0;
+
             //获取员工表
             DataTable dtStaff = objAttRecordService.GetAllStaffsDataSet().Tables[0];
             //获取所有时段
             var iTimesList = objAttRecordService.GetAllTimesList();
+
             //月天数变量
-            var idayNumber = DateTime.Now.Date;//日期的天数,测试变量，实际使用中为查询的日期
+            DateTime idayNumber; //= DateTime.Now.Date;//日期的天数,测试变量，实际使用中为查询的日期
+
+            #region 创建一个Datatable表存放整理好的打卡数据
+
             //缓存表
             DataTable dtAttTemp = new DataTable();
+            //设定列数据
+            dtAttTemp.Columns.Add("SfId", typeof(int));
+            dtAttTemp.Columns.Add("SfName", typeof(string));
+            dtAttTemp.Columns.Add("SfGroup", typeof(string));
+            dtAttTemp.Columns.Add("SfShifts", typeof(string));
+            dtAttTemp.Columns.Add("WorkTime1", typeof(string));
+            dtAttTemp.Columns.Add("OffDutyTime1", typeof(string));
+            dtAttTemp.Columns.Add("WorkTime2", typeof(string));
+            dtAttTemp.Columns.Add("OffDutyTime2", typeof(string));
+            dtAttTemp.Columns.Add("WorkTime3", typeof(string));
+            dtAttTemp.Columns.Add("OffDutyTime3", typeof(string));
+            dtAttTemp.Columns.Add("AtDay", typeof(decimal));
+
+            #endregion
+
+
             //计算日报
             for (int i = 0; i < dtStaff.Rows.Count; i++)
             {
@@ -282,7 +304,6 @@ namespace AttReport
                     //上班时间1
                     TimeSpan WorkTime1 = TimeSpan.Parse(iTimesList
                         .Where(time => time.TimesName == iTimesName1).ToList()[0].WorkTime);
-
                     //下班时间1
                     TimeSpan OffDutyTime1 = TimeSpan.Parse(iTimesList
                         .Where(time => time.TimesName == iTimesName1).ToList()[0].OffDutyTime);
@@ -363,86 +384,111 @@ namespace AttReport
 
                     #endregion
 
-                    #region 重新整理打卡记录                    
+                    #region 重新整理打卡记录，根据Id取单日记录                    
 
                     //计算统计每位员工的考勤记录
                     var AttResult = (from log in dtAttLog.AsEnumerable()//查询集合
-                                    where Convert.ToInt32(log.Field<Int32>("ClockId")) == iSfId//满足id条件
-                                    && Convert.ToDateTime(log.Field<string>("ClockRecord").ToString()).Date == iToday//满足单日条件
-                                    select log.Field<string>("ClockRecord")).ToList();//返回记录集合
+                                     where Convert.ToInt32(log.Field<Int32>("ClockId")) == iSfId//满足id条件
+                                     && Convert.ToDateTime(log.Field<string>("ClockRecord")).Date == iToday//满足单日条件
+                                     select log.Field<string>("ClockRecord")).ToList();//返回记录集合
+                    #endregion
 
-                    /////////////////////////////////
-                    foreach (string item in AttResult)
-                    {
-                        Console.WriteLine(item);
-                        Console.WriteLine("-------------");
-                    }
-                    /////////////////////////////////
-
-                    #region 计算唯一有效记录
-
+                    /////////////////////
                     //创建局部变量接收唯一打卡值
-                    string OnlyWorkTime = "";//唯一打卡值
+                    string OnlyWorkTime1 = "";//上班唯一打卡值1
+                    string OnlyOffDutyTime1 = "";//下班唯一打卡值1
+                    string OnlyWorkTime2 = "";//上班唯一打卡值2
+                    string OnlyOffDutyTime2 = "";//下班唯一打卡值2
+                    string OnlyWorkTime3 = "";//上班唯一打卡值3
+                    string OnlyOffDutyTime3 = "";//下班唯一打卡值3
+                    ////////////////////////////////////////////
 
-                    //创建List集合，用于后面的数据存放
-                    List<string> logList = new List<string>();
+                    //上班打卡1---大于等于开始签到时间，小于下班开始签退时间，包含迟到（取第一）
+                    OnlyWorkTime1 = AttResult.Where(at => DateTime.Parse(at).TimeOfDay >= StartCheckIn1
+                    && DateTime.Parse(at).TimeOfDay < StartSignBack1).FirstOrDefault();
+                    //下班打卡1---大于第一次上班结束签到时间，小于第二次上班开始签到时间，包含早退（取最后）
+                    OnlyOffDutyTime1 = AttResult.Where(at => DateTime.Parse(at).TimeOfDay > EndCheckIn1
+                    && DateTime.Parse(at).TimeOfDay < StartCheckIn2).LastOrDefault();
 
-                    //筛选第一次上班（上午）的有效记录
-                    foreach (var at in AttResult)
+                    //第二次上班
+                    if (WorkTime2 != null && OffDutyTime2 != null)
                     {
-                        if (DateTime.Parse(at).TimeOfDay < WorkTime1)//WorkTime1代表第一次上班时间
+                        //上班打卡2
+                        OnlyWorkTime2 = AttResult.Where(at => DateTime.Parse(at).TimeOfDay >= StartCheckIn2
+                        && DateTime.Parse(at).TimeOfDay < StartSignBack2).FirstOrDefault();
+                        //下班打卡2
+                        OnlyOffDutyTime2 = AttResult.Where(at => DateTime.Parse(at).TimeOfDay > EndCheckIn2
+                        && DateTime.Parse(at).TimeOfDay < StartCheckIn3).LastOrDefault();
+
+                        //处理延迟下午下班加班的异常
+                        if (iClassesName == "办公职员" && OnlyOffDutyTime2.Length == 0)//待修改完善的代码
                         {
-                            //添加进list集合
-                            logList.Add(at);
+                            //获取下班时间后的打卡记录
+                            var OffDutyList = AttResult.Where(at => DateTime.Parse(at).TimeOfDay >= OffDutyTime2
+                            && DateTime.Parse(at).TimeOfDay < psPm).ToList();//psPm=23:59:59
+
+                            //获取最后的打卡记录
+                            if (OffDutyList.Count != 0)
+                            {
+                                //自动签卡，签下班时间
+                                OnlyOffDutyTime2 = DateTime.Parse((iToday + OffDutyTime2).ToString())
+                                    .AddMinutes(3).AddSeconds(33).ToString(); //为下班时间加33分33秒
+                            }
                         }
                     }
 
-                    //筛选唯一记录
-                    if (logList != null)
+                    //第三次上班
+                    if (WorkTime3!=TimeSpan.Zero&& OffDutyTime3 != TimeSpan.Zero)
                     {
-                        if (logList.Count == 1)
+                        //上班打卡3（加班），取大于晚上加班的第一个打卡记录
+                        OnlyWorkTime3 = AttResult.Where(at => DateTime.Parse(at).TimeOfDay >= StartCheckIn3
+                        && DateTime.Parse(at).TimeOfDay < psPm).FirstOrDefault();//psPm=23:59:59
+
+                        //下班打卡3（加班），取晚上加班的最后一个打卡记录
+                        OnlyOffDutyTime3 = AttResult.Where(at => DateTime.Parse(at).TimeOfDay >= StartCheckIn3
+                        && DateTime.Parse(at) < DateTime.Parse(iToday + StartCheckIn1.ToString()).AddDays(1)).LastOrDefault();//次日上班开始签到时间
+                    }
+
+                    //处理两班倒和三班倒加班异常
+                    if (WorkTime2 == TimeSpan.Zero && OffDutyTime2 == TimeSpan.Zero && WorkTime3 == TimeSpan.Zero && OffDutyTime3 == TimeSpan.Zero)
+                    {
+                        //获取下班时间后到次日上班开始签到时间的打卡记录
+                        var OffDutyList = AttResult.Where(at => DateTime.Parse(at).TimeOfDay >= OffDutyTime2
+                        && DateTime.Parse(at) < DateTime.Parse(iToday + StartCheckIn1.ToString()).AddDays(1)).ToList();
+
+                        //获取最后的打卡记录
+                        if (OffDutyList.Count != 0)
                         {
-                            OnlyWorkTime = logList.FirstOrDefault();
-                        }
-                        else if (logList.Count > 1)
-                        {
-                            if (logList.Where(x => DateTime.Parse(x).TimeOfDay >= StartCheckIn1).FirstOrDefault().Count() > 0
-                                && logList.Where(x => DateTime.Parse(x).TimeOfDay >= StartCheckIn1).FirstOrDefault() != null)
-                            {
-                                OnlyWorkTime = logList.Where(x => DateTime.Parse(x).TimeOfDay >= StartCheckIn1).FirstOrDefault();//StartCheckIn1是开始签到时间1
-                            }
-                            else
-                            {
-                                OnlyWorkTime = logList.FirstOrDefault();
-                            }
+                            //自动签卡，签下班时间
+                            OnlyOffDutyTime1 = DateTime.Parse((iToday + OffDutyTime2).ToString())
+                                .AddMinutes(3).AddSeconds(33).ToString(); //为下班时间加33分33秒
                         }
                     }
 
-                    //测试输出
-                    Console.WriteLine(iSfId+" "+iSfName+" "+iSfGroupName+" "+OnlyWorkTime+"\n");                    
-
-                    #endregion
 
 
-                    //获取第一次下班的打卡记录
+                    ///////////////////////////////////
 
-                    #endregion
-
-
-                    ////添加进表
-                    //foreach (var item in Result1)
-                    //{
-                    //    DataRow dr = dtAttTemp.NewRow();
-                    //    dr[0] = iSfId;
-                    //    dr[1] = iSfName;
-                    //    dr[2] = iSfGroupName;
-                    //    dr[3] = item.Field<string>("ClockRecord");
-                    //    dtAttTemp.Rows.Add(dr);
-                    //}
+                    //存放数据
+                    DataRow dr = dtAttTemp.NewRow();
+                    dr[0] = iSfId;
+                    dr[1] = iSfName;
+                    dr[2] = iSfGroupName;
+                    dr[3] = iClassesName;
+                    dr[4] = OnlyWorkTime1;
+                    dr[5] = OnlyOffDutyTime1;
+                    dr[6] = OnlyWorkTime2;
+                    dr[7] = OnlyOffDutyTime2;
+                    dr[8] = OnlyWorkTime3;
+                    dr[9] = OnlyOffDutyTime3;
+                    dr[10] = 0;
+                    dtAttTemp.Rows.Add(dr);
                 }
             }
+
             Console.ReadLine();
         }
+
         #endregion
 
         //员工入职窗口对象
